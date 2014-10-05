e296b6171b94649af22ac7c830e6bb562609deac
Blackburn29 <blackburn2929@gmail.com>
2014-10-05 01:20:03 -0500

    Add necessary gen files for eclipse

--- gen/net/getatraci/atraci/R.java
+++ gen/net/getatraci/atraci/R.java
@@ -9,9 +9,12 @@ package net.getatraci.atraci;
 
 public final class R {
     public static final class array {
-        public static final int navigation_items=0x7f070000;
+        public static final int navigation_items=0x7f080000;
     }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int grey=0x7f050000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -21,9 +24,9 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
-        public static final int font_size=0x7f050002;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
+        public static final int font_size=0x7f060002;
     }
     public static final class drawable {
         public static final int afro=0x7f020000;
@@ -42,49 +45,67 @@ public final class R {
         public static final int summer=0x7f02000d;
     }
     public static final class id {
-        public static final int action_search=0x7f0a000b;
-        public static final int action_settings=0x7f0a000c;
-        public static final int album_art=0x7f0a0009;
-        public static final int drawer_layout=0x7f0a0000;
-        public static final int gridview=0x7f0a0004;
-        public static final int item1=0x7f0a000a;
-        public static final int left_drawer=0x7f0a0001;
-        public static final int lyrics_box=0x7f0a0003;
-        public static final int menuRefresh=0x7f0a000d;
-        public static final int picture=0x7f0a0006;
-        public static final int play_btn=0x7f0a0008;
-        public static final int search_list=0x7f0a0005;
-        public static final int text=0x7f0a0007;
-        public static final int youtube_view=0x7f0a0002;
+        public static final int action_search=0x7f0b0011;
+        public static final int action_settings=0x7f0b0012;
+        public static final int album_art=0x7f0b000f;
+        public static final int commit_list=0x7f0b000b;
+        public static final int commit_message=0x7f0b000a;
+        public static final int content_frame=0x7f0b0001;
+        public static final int drawer_layout=0x7f0b0000;
+        public static final int gridview=0x7f0b0008;
+        public static final int item1=0x7f0b0010;
+        public static final int left_drawer=0x7f0b0002;
+        public static final int lyrics_box=0x7f0b0007;
+        public static final int menuRefresh=0x7f0b0013;
+        public static final int nextbut=0x7f0b0006;
+        public static final int picture=0x7f0b000c;
+        public static final int play_btn=0x7f0b000e;
+        public static final int playbut=0x7f0b0005;
+        public static final int prevbut=0x7f0b0004;
+        public static final int search_list=0x7f0b0009;
+        public static final int text=0x7f0b000d;
+        public static final int youtube_view=0x7f0b0003;
     }
     public static final class layout {
         public static final int actionbar_indeterminate_progress=0x7f030000;
         public static final int activity_home=0x7f030001;
         public static final int activity_player=0x7f030002;
-        public static final int activity_postsearchsonglist=0x7f030003;
-        public static final int activity_search=0x7f030004;
-        public static final int drawer_list_item=0x7f030005;
-        public static final int musicgrid_item=0x7f030006;
-        public static final int searchlist_item=0x7f030007;
+        public static final int activity_playlist_selector=0x7f030003;
+        public static final int activity_postsearchsonglist=0x7f030004;
+        public static final int activity_search=0x7f030005;
+        public static final int commit_item=0x7f030006;
+        public static final int drawer_list_item=0x7f030007;
+        public static final int fragment_homenews=0x7f030008;
+        public static final int musicgrid_item=0x7f030009;
+        public static final int searchlist_item=0x7f03000a;
     }
     public static final class menu {
-        public static final int home=0x7f090000;
-        public static final int player=0x7f090001;
-        public static final int postsearch=0x7f090002;
-        public static final int searchview=0x7f090003;
+        public static final int home=0x7f0a0000;
+        public static final int player=0x7f0a0001;
+        public static final int playlist_selector=0x7f0a0002;
+        public static final int postsearch=0x7f0a0003;
+        public static final int searchview=0x7f0a0004;
     }
     public static final class string {
-        public static final int action_search=0x7f060003;
-        public static final int action_settings=0x7f060002;
-        public static final int album_thumbnail=0x7f06000a;
-        public static final int app_name=0x7f060000;
-        public static final int drawer_close=0x7f060009;
-        public static final int drawer_open=0x7f060008;
-        public static final int hello_world=0x7f060001;
-        public static final int lyrics=0x7f060007;
-        public static final int menuitem_refresh=0x7f060006;
-        public static final int seach_hint=0x7f060004;
-        public static final int title_activity_player=0x7f060005;
+        public static final int action_search=0x7f070003;
+        public static final int action_settings=0x7f070002;
+        public static final int album_thumbnail=0x7f070012;
+        public static final int app_name=0x7f070000;
+        public static final int backarrow=0x7f07000c;
+        public static final int drawer_close=0x7f070009;
+        public static final int drawer_open=0x7f070008;
+        public static final int forwardarrow=0x7f07000d;
+        public static final int hello_world=0x7f070001;
+        public static final int lyrics=0x7f070007;
+        public static final int menuitem_refresh=0x7f070006;
+        public static final int pause=0x7f07000b;
+        public static final int play=0x7f07000a;
+        public static final int playlists=0x7f07000f;
+        public static final int seach_hint=0x7f070004;
+        public static final int title_activity_player=0x7f070005;
+        public static final int title_activity_playlist_selector=0x7f07000e;
+        public static final int top100=0x7f070010;
+        public static final int top_tracks=0x7f070011;
     }
     public static final class style {
         /** 
@@ -108,11 +129,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f080000;
+        public static final int AppBaseTheme=0x7f090000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f080001;
+        public static final int AppTheme=0x7f090001;
     }
     public static final class xml {
         public static final int searchable=0x7f040000;
